c33e7c19bc231b9ed5300d51f87077bc0630d3b3
1Fire4 <wangdingyi2@huawei.com>
2025-10-25 09:50:41 +0800

    [Fea] support FULL_DECODE_ONLY mode for DeepSeekv3.2-Exp
    Signed-off-by: 1Fire4 <wangdingyi2@huawei.com>

--- vllm_ascend/attention/sfa_v1.py
+++ vllm_ascend/attention/sfa_v1.py
@@ -167,7 +167,7 @@ M = TypeVar("M", bound=AscendSFAMetadata)
 class AscendSFAMetadataBuilder:
     # Does this backend/builder support ACL Graphs for attention (default: no).
     aclgraph_support: ClassVar[AttentionCGSupport] = \
-        AttentionCGSupport.NEVER
+        AttentionCGSupport.UNIFORM_SINGLE_TOKEN_DECODE
     """
     NOTE: Please read the comment at the top of the file before trying to
     understand this class
@@ -437,6 +437,26 @@ class AscendSFAMetadataBuilder:
             enable_dbo_across_dp=common_attn_metadata.enable_dbo_across_dp,
         )
 
+    def build_for_graph_capture(
+        self,
+        common_attn_metadata: AscendCommonAttentionMetadata,
+        attn_state: AscendAttentionState = AscendAttentionState.DecodeOnly,
+        model: Optional[nn.Module] = None,
+    ):
+        if attn_state == AscendAttentionState.DecodeOnly:
+            attn_metadata = self.build(
+                common_prefix_len=0,
+                common_attn_metadata=common_attn_metadata,
+                model=model,
+            )
+        else:
+            raise NotImplementedError(
+                "Currently we only support building dummy metadata for DecodeOnly state"
+            )
+
+        attn_metadata.attn_state = attn_state
+        return attn_metadata
+    
 
 class PrefillSFAPreprocessResult(NamedTuple):
     q_nope: Optional[torch.Tensor] = None
